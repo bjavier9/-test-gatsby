6ffd4ecd2edd404a2040fd8cc1b85b6889b04b4f
Javier Balsas <javier9518@gmail.com>
2020-06-28 17:59:18 -0500

    Test Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "purple" }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
+      <img
+        height="300"
+        src="https://media.metrolatam.com/2020/04/28/miakhalifaexactr-80d5e02e187b129353354c7853d22059-1200x800.jpg"
+        alt="hola mundo"
+      />
+    </div>
+  )
 }
